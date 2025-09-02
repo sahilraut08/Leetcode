2d632f71bbb7bffbaf0ec02e691394ca01f00231
Sahil Raut <68119412+sahilraut08@users.noreply.github.com>
2025-09-01 19:48:35 -0700

    Time: 0 ms (100%), Space: 41.6 MB (87.3%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.java
@@ -0,0 +1,41 @@
+class Solution {
+    int [][] dirs;
+    public void gameOfLife(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        this.dirs = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1,1}}; 
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                int liveCells = countLive(board, i, j, m, n);
+                if(board[i][j] == 1 && (liveCells < 2 || liveCells > 3)) {
+                    board[i][j] = 3;
+                } 
+                else if(board[i][j] == 0 && liveCells == 3) {
+                    board[i][j] = 2;
+                }
+            }
+        }
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == 3) {
+                    board[i][j] = 0;
+                } else if (board[i][j] == 2) {
+                    board[i][j] = 1;
+                }
+             }
+        }     
+    }
+
+    private int countLive(int[][] board, int i, int j, int m, int n) {
+        int result = 0;
+        for(int [] dir : dirs){
+            int row = i + dir[0];
+            int col = j + dir[1];
+            if(row >= 0 && col >= 0 && row < m && col < n && (board[row][col] == 1 || board[row][col] == 3)) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+}
