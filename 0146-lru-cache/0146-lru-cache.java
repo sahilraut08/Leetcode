d114eaf0fe34ce854de3cd67c668f75991cdc8b3
Sahil Raut <68119412+sahilraut08@users.noreply.github.com>
2025-09-01 19:38:58 -0700

    Time: 42 ms (92.24%), Space: 113.3 MB (64.53%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,71 @@
+class LRUCache {
+
+    class Node {
+        int val;
+        int key;
+        Node prev;
+        Node next;
+        public Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    Node head;
+    Node tail;
+    HashMap<Integer, Node> map;
+    int capacity;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.head = new Node(-1, -1);
+        this.tail = new Node(-1, -1);
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+        this.map = new HashMap<>();
+    }
+    
+    private void addToHead(Node node) {
+        node.next = head.next;
+        node.prev = head;
+        head.next = node;
+        node.next.prev = node;
+    }
+
+    private void removeNode(Node node) {
+        node.next.prev = node.prev;
+        node.prev.next = node.next;
+    }
+
+    public int get(int key) {
+        if(!map.containsKey(key)) return -1;
+        Node node = map.get(key);
+        removeNode(node);
+        addToHead(node);
+        return node.val;
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key)) {
+            Node node = map.get(key);
+            node.val = value;
+            removeNode(node);
+            addToHead(node);
+        } else {
+            if(map.size() == capacity) {
+                Node tailPrev = tail.prev;
+                removeNode(tailPrev);
+                map.remove(tailPrev.key);
+            } 
+            Node newNode = new Node(key, value);
+            addToHead(newNode);
+            map.put(key, newNode);
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
