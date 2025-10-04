eb6f6a9fd5f97b4b0c41752369bcdd872b4e4e8a
Sahil Raut <68119412+sahilraut08@users.noreply.github.com>
2025-10-04 16:17:24 -0700

    Time: 0 ms (100%), Space: 41 MB (13.13%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -1,25 +1,21 @@
 class Solution {
     public int maximumSwap(int num) {
-        // Convert num to string
         char[] s = Integer.toString(num).toCharArray();
-        
+        int[] last_idx = new int[10];
+
+        for (int i = 0; i < s.length; i++) 
+            last_idx[s[i] - '0'] = i;
+
         for (int i = 0; i < s.length; i++) {
-            int ind = i;
-            
-            for (int j = s.length - 1; j > i; j--) {
-                if (s[ind] < s[j]) {
-                    ind = j;
+            for (int d = 9; d > s[i] - '0'; d--) {
+                if (last_idx[d] > i) {
+                    char temp = s[i];
+                    s[i] = s[last_idx[d]];
+                    s[last_idx[d]] = temp;
+                    return Integer.parseInt(new String(s));
                 }
             }
-
-            if (ind != i && s[i] < s[ind]) {
-                char temp = s[i];
-                s[i] = s[ind];
-                s[ind] = temp;
-                return Integer.parseInt(new String(s));
-            }
         }
-
         return num;
     }
 }
