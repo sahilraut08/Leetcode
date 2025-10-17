b019491f0df58d2d6acd7da96e86e004750a046b
Sahil Raut <68119412+sahilraut08@users.noreply.github.com>
2025-10-17 15:54:02 -0700

    Time: 6 ms (96.33%), Space: 57 MB (90.56%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -4,12 +4,17 @@ class Solution {
         int max = 0;
         int m = matrix.length;
         int n = matrix[0].length;
-        int [][] dp = new int[m+1][n+1];
+        int [] dp = new int[n+1];
         for(int i = 1; i <= m; i++) {
+            int diagUp = 0;
             for(int j = 1; j <= n; j++) {
                 if(matrix[i-1][j-1] == '1') {
-                    dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i-1][j-1], dp[i][j-1]));
-                    max = Math.max(max, dp[i][j]);
+                    int temp = dp[j];
+                    dp[j] = 1 + Math.min(dp[j], Math.min(diagUp, dp[j-1]));
+                    max = Math.max(max, dp[j]);
+                    diagUp = temp;
+                } else {
+                    dp[j] = 0;
                 }
             }
         }
