4335e6b2723ef2a6f139117fb1dfa4e417eac43b
Sahil Raut <68119412+sahilraut08@users.noreply.github.com>
2025-10-21 23:40:29 -0700

    Time: 34 ms (45.2%), Space: 49.3 MB (26.23%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        Map<String, List<String>> graph = new HashMap<>();
+        Map<String, String> emailToName = new HashMap<>();
+
+        for (List<String> acc : accounts) {
+            String name = acc.get(0);
+            for (int i = 1; i < acc.size(); i++) {
+                String email = acc.get(i);
+                emailToName.put(email, name);
+                graph.putIfAbsent(email, new ArrayList<>());
+
+                if (i > 1) {
+                    graph.get(acc.get(1)).add(email);
+                    graph.get(email).add(acc.get(1));
+                }
+            }
+        }
+
+        Set<String> visited = new HashSet<>();
+        List<List<String>> res = new ArrayList<>();
+
+        for (String email : graph.keySet()) {
+            if (!visited.contains(email)) {
+                List<String> merged = new ArrayList<>();
+                Queue<String> q = new LinkedList<>();
+                q.offer(email);
+                visited.add(email);
+
+                while (!q.isEmpty()) {
+                    String cur = q.poll();
+                    merged.add(cur);
+                    for (String next : graph.get(cur)) {
+                        if (visited.add(next)) {
+                            q.offer(next);
+                        }
+                    }
+                }
+
+                Collections.sort(merged);
+                merged.add(0, emailToName.get(email));
+                res.add(merged);
+            }
+        }
+
+        return res;
+    }
+}
