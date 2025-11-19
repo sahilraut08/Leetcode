9ef57643e3ab63c1f65b0b1e9ac07feb45fd1ba3
Sahil Raut <68119412+sahilraut08@users.noreply.github.com>
2025-11-18 16:39:25 -0800

    Time: 360 ms (38.67%), Space: 98.3 MB (16.35%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
@@ -1,32 +1,56 @@
 class Solution {
-    public int minCostConnectPoints(int[][] points) {
-        int n = points.length;
 
-        boolean[] visited = new boolean[n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0]-b[0]);
-
-        pq.add(new int[]{0,0});
-        int totalCost = 0;
-        int edges = 0;
-
-        while(edges < n) {
-            int[] curr = pq.poll();
-            int cost = curr[0];
-            int u = curr[1];
-
-            if(visited[u]) continue;
-            visited[u] = true;
-            totalCost += cost;
-            edges++;
-
-            for(int v = 0; v < n; v++) {
-                if(!visited[v]) {
-                    int w = Math.abs(points[u][0] - points[v][0]) 
-                            + Math.abs(points[u][1] - points[v][1]);
-                    pq.add(new int[]{w, v});
-                }
+    class DSU {
+        int[] parent, size;
+        public DSU(int n) {
+            parent = new int[n];
+            size = new int[n];
+            for(int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
             }
         }
-        return totalCost;
+        public int find(int x) {
+            if(parent[x] != x) parent[x] = find(parent[x]);
+            return parent[x];
+        }
+        public void union(int x, int y) {
+            int px = find(x), py = find(y);
+            if(px == py) return;
+            if(size[px] < size[py]) {
+                parent[px] = py;
+                size[py] += size[px];
+            } else {
+                parent[py] = px;
+                size[px] += size[py];
+            }
+        }
+    }
+
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        List<int[]> edges = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int w = Math.abs(points[i][0] - points[j][0])
+                      + Math.abs(points[i][1] - points[j][1]);
+                edges.add(new int[]{i,j,w});
+            }
+        }
+
+        edges.sort((a, b) -> a[2] - b[2]);
+
+        DSU dsu = new DSU(n);
+        int cost = 0;
+
+        for (int[] e : edges) {
+            if (dsu.find(e[0]) != dsu.find(e[1])) {
+                dsu.union(e[0], e[1]);
+                cost += e[2];
+            }
+        }
+
+        return cost;
     }
 }
